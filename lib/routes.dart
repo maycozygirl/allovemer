91ce03effe6e06a2bb17996f5f780ce7f00fd823
maycozygirl <may11682.o@gmail.com>
2022-11-20 07:39:32 +0700

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:al_lover_mer/screen/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder> {
+  "/login": (BuildContext context) => LoginScreen(),
+  
+
+};
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/bg_Signin.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
+      ),
+    );
+  }
+}
