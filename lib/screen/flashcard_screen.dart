e231abae1781bfb72d0253f79a03fe1ecf71f4b6
fahh11 <phaptawan2546@gmail.com>
2022-11-21 14:44:59 +0700

    flashcard

---
+++ lib/screen/flashcard_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FlashcardScreen extends StatefulWidget {
+  const FlashcardScreen({super.key});
+
+  @override
+  State<FlashcardScreen> createState() => _FlashcardScreenState();
+}
+
+class _FlashcardScreenState extends State<FlashcardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bg_FlashCard.jpg"),
+                fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
