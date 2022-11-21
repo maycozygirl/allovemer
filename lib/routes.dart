b13968fd89897a38039a64401f777ae9460098ac
maycozygirl <may11682.o@gmail.com>
2022-11-21 15:45:20 +0700

    bg

---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:al_lover_mer/screen/flashcard_screen.dart';
+import 'package:al_lover_mer/screen/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  "/login": (BuildContext context) => LoginScreen(),
+  "/flashcard": (BuildContext context) => FlashcardScreen(),
+};
