b84cd7c0bc656a005e19834aeea80442dea93b33
maycozygirl <may11682.o@gmail.com>
2022-11-22 19:57:53 +0700

    diary2

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:al_lover_mer/screen/diary_screen.dart';
 import 'package:al_lover_mer/screen/flashcard_screen.dart';
 import 'package:al_lover_mer/screen/help_screen.dart';
 import 'package:al_lover_mer/screen/home_screen.dart';
@@ -13,4 +14,5 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/home": (BuildContext context) => HomeScreen(),
   "/medicine": (BuildContext context) => MedicineScreen(),
   "/logoscreen": (BuildContext context) => LogoScreen(),
+  "/diary": (BuildContext context) => DiaryScreen(),
 };
