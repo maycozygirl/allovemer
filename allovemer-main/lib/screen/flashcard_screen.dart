475645b85a8518afda1501e247350cf44494a13c
plbkppexo <worapha.c@ku.th>
2022-11-22 11:15:29 +0700

    praew

---
+++ allovemer-main/lib/screen/flashcard_screen.dart
@@ -0,0 +1,94 @@
+import 'package:al_lover_mer/Themes/colors.dart';
+import 'package:flutter/material.dart';
+
+class FlashcardScreen extends StatefulWidget {
+  const FlashcardScreen({super.key});
+
+  @override
+  State<FlashcardScreen> createState() => _FlashcardScreenState();
+}
+
+class _FlashcardScreenState extends State<FlashcardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover)),
+      child: Padding(
+          padding: EdgeInsets.only(top: 100),
+          child: Center(
+            child: Container(
+                width: 350,
+                height: 730,
+                decoration: BoxDecoration(
+                  color: kColorYellow,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                  child: Column(
+                    children: [
+                      Text("เกมบัตรคำ", style: TextStyle(fontFamily: 'TH-Chara',fontSize: 40),),
+                      Expanded(
+                      child: GridView.count(
+                        primary: false,
+                        padding: const EdgeInsets.all(20),
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        crossAxisCount: 2,
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            padding: const EdgeInsets.all(8),
+                            child: const Text(
+                              "ผลไม้",
+                              style: TextStyle(fontFamily: 'TH-Chara', fontSize: 50),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            padding: const EdgeInsets.all(8),
+                            child: const Text(
+                              "ครอบครัวของฉัน",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontFamily: 'TH-Chara', fontSize: 35,),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),],
+                  ),
+                )),
+          )),
+    ),
+    floatingActionButton: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: buildAddButton(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+
+Widget buildAddButton() => FloatingActionButton.extended(
+      backgroundColor: kColorLightYellow,
+      label: Text(
+        'เพิ่มยาประจำตัว',
+        style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w300,
+            fontFamily: 'TH-Chara',
+            fontSize: 30),
+      ),
+      onPressed: () {},
+    );
+   
---
+++ allovemer-main/lib/screen/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:al_lover_mer/Themes/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bg_Home.jpg"), fit: BoxFit.cover)),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Custom_button(
+              text: 'เกมบัตรคำ',
+              onPressed: () {
+                Navigator.pushNamed(context, "/flashcard");
+              },
+              backgroundColor: kColorYellow,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Custom_button(
+                text: 'ยาประจำตัว',
+                backgroundColor: kColorGreen,
+                onPressed: () {
+                  Navigator.pushNamed(context, "/medicine");
+                }),
+            SizedBox(
+              height: 30,
+            ),
+            Custom_button(
+                text: 'วันสำคัญ',
+                backgroundColor: kColorPurple,
+                onPressed: () {}),
+            SizedBox(
+              height: 30,
+            ),
+            Custom_button(
+                text: 'ติดต่อขอความช่วยเหลือ',
+                backgroundColor: kColorRed,
+                onPressed: () {
+                  Navigator.pushNamed(context, "/help");
+                })
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class Custom_button extends StatelessWidget {
+  const Custom_button(
+      {Key? key,
+      required this.text,
+      required this.backgroundColor,
+      required this.onPressed})
+      : super(key: key);
+
+  final String text;
+  final Color backgroundColor;
+  final Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        height: 150,
+        width: 300,
+        decoration: BoxDecoration(
+            color: backgroundColor, borderRadius: BorderRadius.circular(45)),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
