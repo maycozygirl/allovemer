9ed71166b47bd4504bccf59b4d68dbf60200864a
plbkppexo <worapha.c@ku.th>
2022-11-22 17:09:19 +0700

    diary

---
+++ allovemer-main/lib/model/note_model.dart
@@ -0,0 +1,10 @@
+class Note {
+  final String? creation_date;
+  final String? note_content;
+  final String? note_title;
+
+  Note(
+      {required this.creation_date,
+      required this.note_content,
+      required this.note_title});
+}
--- allovemer-main/lib/routes.dart
+++ allovemer-main/lib/routes.dart
@@ -1,10 +1,12 @@
+import 'package:al_lover_mer/screen/diary_screen.dart';
 import 'package:al_lover_mer/screen/flashcard_screen.dart';
 import 'package:al_lover_mer/screen/help_screen.dart';
 import 'package:al_lover_mer/screen/home_screen.dart';
-import 'package:al_lover_mer/screen/login_screen.dart';
 import 'package:al_lover_mer/screen/logo_screen.dart';
 import 'package:al_lover_mer/screen/medicine_screen.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'screen/login_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/login": (BuildContext context) => LoginScreen(),
@@ -13,4 +15,5 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/home": (BuildContext context) => HomeScreen(),
   "/medicine": (BuildContext context) => MedicineScreen(),
   "/logoscreen": (BuildContext context) => LogoScreen(),
+  "/diary": (BuildContext context) => DiaryScreen(),
 };
