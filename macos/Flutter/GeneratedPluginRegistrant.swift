5820b62c783cb2ed1651372995aaabc907e21179
maycozygirl <may11682.o@gmail.com>
2022-11-24 00:17:05 +0700

    fix: edit logfile

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,19 +8,13 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
-<<<<<<< HEAD
 import firebase_storage
-=======
->>>>>>> b2a7e08bb0f0b07d11c1f01932e1f84bd0e754dd
 import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-<<<<<<< HEAD
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-=======
->>>>>>> b2a7e08bb0f0b07d11c1f01932e1f84bd0e754dd
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
 }
